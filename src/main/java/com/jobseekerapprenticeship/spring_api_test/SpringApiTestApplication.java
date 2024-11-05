3fe21631156451610d70202d32050b5b41b632bd
hezbims <hezbisulaiman@gmail.com>
2024-11-05 15:35:12 +0800

    commit pertama

---
+++ src/main/java/com/jobseekerapprenticeship/spring_api_test/SpringApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.jobseekerapprenticeship.spring_api_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobseekerapprenticeship/spring_api_test/SpringApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseekerapprenticeship.spring_api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
