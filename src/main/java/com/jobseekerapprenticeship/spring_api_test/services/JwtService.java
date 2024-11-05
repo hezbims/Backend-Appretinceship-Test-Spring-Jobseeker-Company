f3d1ae83a5932b34b0257f7fe636c3bfcc949f0b
hezbims <hezbisulaiman@gmail.com>
2024-11-05 16:28:54 +0800

    feat: nambahkan jwtservice untuk encoding dan decoding jwt

---
+++ src/main/java/com/jobseekerapprenticeship/spring_api_test/services/JwtService.java
@@ -0,0 +1,64 @@
+package com.jobseekerapprenticeship.spring_api_test.services;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtService {
+    private static final String SECRET = "608ebd5299e28f92a7c645f7ade96ed125b4c0e9c8858015eab1274d963ec96262a91951638ba81d44ed0d19f1231d3db26cb5229879d3545f0b6e5e8565d9f3";
+
+    /**
+     * Mendapatkan username apabila token valid, return null apabila token tidak valid
+     * @param token JWT Token
+     */
+    public String getUsername(String token){
+        if (isExpired(token))
+            return null;
+        return getClaim(token, Claims::getSubject);
+    }
+
+    private boolean isExpired(final String token){
+        final Date expirationDate = getClaim(token, Claims::getExpiration);
+        return new Date(System.currentTimeMillis()).after(expirationDate);
+    }
+
+    private <T> T getClaim(String token, Function<Claims, T> resolver){
+        return resolver.apply(getAllClaims(token));
+    }
+
+    private Claims getAllClaims(String token){
+        final JwtParser jwtParser = Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build();
+
+        return jwtParser
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSecretKey(){
+        final byte[] keyBytes = Base64.getDecoder().decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
+    public String generateJwt(String username){
+        final Date currentTime = new Date(System.currentTimeMillis());
+        final Date expirationTime = new Date(currentTime.getTime() + 1000 * 3600 * 24 * 7);
+
+        return Jwts
+                .builder()
+                .subject(username)
+                .issuedAt(currentTime)
+                .expiration(expirationTime)
+                .signWith(getSecretKey())
+                .compact();
+    }
+}
